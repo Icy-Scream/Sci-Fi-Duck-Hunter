ed98dcdc4404fae78bbbcd216da0891beacfde73
Icy-Scream <jdpetta21@gmail.com>
2023-01-25 10:07:58 -1000

    AI Moves From Start Point To End Point

--- Assets/Script/AI.cs
+++ Assets/Script/AI.cs
@@ -16,12 +16,13 @@ public class AI : MonoBehaviour
 
     void Start()
     {
-        _agent.destination = Waypoints[1].transform.position;
+        _agent.destination = Waypoints[0].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!_agent.pathPending && _agent.remainingDistance < 0.5f)
+            _agent.destination = Waypoints[1].transform.position;
     }
 }
